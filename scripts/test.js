45f9e5cea15725cd9f460be06281ac963d0b7069
thananauto <thananjayan1988@gmail.com>
2023-05-25 16:26:00 +0530

    Added path

--- scripts/test.js
+++ scripts/test.js
@@ -10,8 +10,8 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 //init method to set the virtual users
 export const options ={
     stages :[
-        { duration: '30s', target: 8 },
-        { duration: '15s', target: 5 },
+        { duration: '15s', target: 8 },
+        { duration: '10s', target: 5 },
         { duration: '5s', target: 0 },
     ]
    // vus: 10,
@@ -29,9 +29,9 @@ export function handleSummary(data) {
  
   return {
       'stdout': textSummary(data, { indent: ' ', enableColors: true}), // Show the text summary to stdout...
-      './result/junit.xml': jUnit(data), // but also transform it and save it as a JUnit XML...
-      './result/summary.json': JSON.stringify(data), // and a JSON with all the details...
-      "./result/index.html": htmlReport(data),
+      '.\result\junit.xml': jUnit(data), // but also transform it and save it as a JUnit XML...
+      '.\result\summary.json': JSON.stringify(data), // and a JSON with all the details...
+      ".\result\index.html": htmlReport(data),
       //'./xrayJunit.xml': generateXrayJUnitXML(data, 'summary.json', encoding.b64encode(JSON.stringify(data))),
       // And any other JS transformation of the data you can think of,
       // you can write your own JS helpers to transform the summary data however you like!
